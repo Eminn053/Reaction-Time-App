fa21fc0f87886958831e500368ba27c1533bea8b
Eminn053 <62152617+Eminn053@users.noreply.github.com>
2020-04-04 23:54:48 +0300

    Added comments.

--- Reaction_App/Program.cs
+++ Reaction_App/Program.cs
@@ -8,20 +8,27 @@ namespace Reaction_App
         static void react()
         {
 
-            Console.WriteLine("Press A"); 
+            Console.WriteLine("Press A");
+
+            //The time user presses a will be saved for comparison. 
             if(Console.ReadLine() == "a")
-            {
+            { 
+                //The calculation will go here.(I don't know how to calculate reaction time)
+
                 Console.Write("You pressed A");
 
             }
             Thread.Sleep(500);
-            for (int  i=0;i<=100;i+=2)
+
+            for (int  i=0;i<=100;i+=2)//This is just for testing purposes.
             {
                 Console.WriteLine(i);
             }
         }
         static void Main(string[] args)
         {
+
+            //Basic functions go here.
             int MillisecondsTimeout = 2300;
             Console.WriteLine("Welcome to Reaction tester.");
             Console.WriteLine("Rules:");
